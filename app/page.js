2ad1c4210cffb2c84d36003a24554b4b27e6f0aa
Mukul Chauhan <mukulchauhan64976@gmail.com>
2025-02-26 15:48:50 +0530

    upload portfolio

--- app/page.js
+++ app/page.js
@@ -1,101 +1,122 @@
-import Image from "next/image";
+"use client"
+import React from 'react';
+import Link from 'next/link';
+import { Button } from "@/components/ui/button";
+import Typed from 'typed.js';
+import { useEffect,useRef ,useState} from 'react';
+import Image from 'next/image';
 
-export default function Home() {
+function Home() {
+  const el =useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["WebDeveloper","FrontEnd Developer","BackEnd Developer","FullStack Developer","MernStack Developer"],
+      typeSpeed: 50,
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+   <div >
+  {/* Hero Section */}
+  <section className=" py-20">
+    <div className="container mx-auto px-6 text-center">
+      <div className="flex flex-col md:flex-row items-center justify-center">
+        <div className="w-full md:w-1/2 flex justify-center">
+          <Image
+            src="/WhatsApp Image 2025-02-26 at 13.23.01_3bd9274b.jpg"
+            alt="Profile"
+            height={'300'}
+            width={'300'}
+            className="w-full md:w-1/2 rounded-full border-purple-400 border-4 mb-4"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="w-full md:w-2/3 lg:w-1/2">
+          <h1 className="text-4xl font-bold mb-4 text-purple-500">I am a <span  ref={el}></span></h1>
+          <p className="text-xl mb-2">Passionate Full-Stack Developer with expertise in front-end design, back-end development, and scalable web solutions I create impactful, efficient websites and applications that bring ideas to life Lets collaborate to turn your vision into reality</p>
+          <h3 className="text-xl font-semibold mb-4 text-purple-500">SKILLS JAVA,HTML,CSS,JAVASCRIPT,EXPRESS JS,MONGODB,SQL,GIT,GITHUB,REACT JS,NEXT JS,BOOTSTRAP,TAILWIND,MATERIAL UI</h3>
+          <div className="flex justify-center gap-6">
+          <Button asChild>
+       <Link href="/contact">Contact</Link>
+          </Button>
+          <Button asChild>
+            <Link href="/project">Project</Link>
+          </Button>
+          </div>
+        </div>
+      </div>
     </div>
+  </section>
+</div>
+      {/* Projects Section */}
+      <section id="projects" className="py-20">
+      <div className="container mx-auto px-6">
+      <h2 className="text-4xl text-purple-500 font-bold text-center mb-12">My Top 3 Projects</h2>
+      <div className="flex flex-wrap gap-6 justify-center">
+        <div className=" rounded-lg flex flex-col justify-center items-center shadow-lg p-6 w-96 transform transition-transform duration-300 hover:translate-y-[-10px] hover:shadow-2xl hover:border hover:border-purple-400 dark:border border-gray-600">
+          <Image src="/Screenshot 2024-09-07 175435.png" alt="Project 1" width={'400'} height={'192'} className="w-full h-48 object-cover rounded-t-lg" />
+          <h3 className="text-2xl font-semibold text-purple-500 mt-4">Wanderlust</h3>
+          <p className=" mt-2 mb-4 text-center">Developed a hotel listing platform with features for browsing,filtering,lisiting hotel,integrating user accounts,and customer review stytem for best user experience.<b>Using Express js and Ejs Templet and more npm pakages(Passport Multter cloudinary)</b>.</p>
+          <Button asChild>
+       <Link href="https://wanderlust-gxsq.onrender.com/listings">View Projects</Link>
+          </Button>
+        </div>
+        <div className=" rounded-lg flex flex-col justify-center items-center shadow-lg p-6 w-96 transform transition-transform duration-300 hover:translate-y-[-10px] hover:shadow-2xl hover:border hover:border-purple-400 dark:border  border-gray-600">
+          <Image src="/Screenshot 2025-02-26 142623.png" alt="Project 2"  width={'400'} height={'192'} className="w-full h-48 object-cover rounded-t-lg" />
+          <h3 className="text-2xl font-semibold text-purple-500 mt-4">My Dairy</h3>
+          <p className="mt-2 mb-4">
+  Developed a platform for customers to browse and purchase fresh dairy products with secure transactions and easy account management. <b>Built with Express.js, React.js, Auth.js, Multer, Cloudinary, Razorpay API, and MongoDB</b>.
+</p>
+
+          <Button asChild>
+       <Link href="/project">View Projects</Link>
+          </Button>
+        </div>
+        <div className=" rounded-lg flex flex-col justify-center items-center shadow-lg p-6 w-96 transform transition-transform duration-300 hover:translate-y-[-10px] hover:shadow-2xl hover:border hover:border-purple-400 dark:border  border-gray-600">
+          <Image src="/Screenshot 2025-02-26 141730.png" alt="Project 2"  width={'400'} height={'192'} className="w-full h-48 object-cover rounded-t-lg" />
+          <h3 className="text-2xl font-semibold text-purple-500 mt-4">Music Player</h3>
+          <p className="mt-2 mb-4">
+  Developed and managed a music player, AsadPlayer. Users can listen to music, play, pause, and toggle between songs. 
+  <b>Built using HTML, CSS, JavaScript, and an external API to fetch songs functionality powered by JavaScript (for play/pause, song switching)</b>
+</p>
+
+          <Button asChild>
+       <Link href="#">View Projects</Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+      </section>
+      <section id="contact" className="py-20">
+      <div className="container mx-auto px-6">
+        <h2 className="text-4xl font-bold mb-8 text-purple-500 text-center">Get In Touch</h2>
+        <div className="max-w-lg mx-auto p-8 flex gap-5 justify-center align-middle rounded-lg shadow-lg w-96 text-center">
+          <div className="mb-4">
+            <a href="mailto:mukulchauhan64976@gmail.com" className="text-blue-500 hover:underline">
+              <i className="fas fa-envelope fa-2x"></i>
+            </a>
+          </div>
+          <div className="mb-4">
+            <a href="https://www.instagram.com/mukulrajput________/" target="_blank" rel="noopener noreferrer" className="text-pink-500 hover:underline">
+              <i className="fab fa-instagram fa-2x"></i>
+            </a>
+          </div>
+          <div className="mb-4">
+            <a href="https://www.linkedin.com/in/mukul-kumar-328a5824b/" target="_blank" rel="noopener noreferrer" className="text-blue-700 hover:underline">
+              <i className="fab fa-linkedin fa-2x"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+      </>
   );
 }
+
+export default Home;
+
